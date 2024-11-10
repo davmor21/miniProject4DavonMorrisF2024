f2e9105c9f2084f386ad6ab5487c52609f145928
Davon Morris <rootbeer587@gmail.com>
2024-11-09 19:22:43 -0600

    Configure models.py

--- tcgApp/cards/models.py
+++ tcgApp/cards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+    collection_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Card(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    card_name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
